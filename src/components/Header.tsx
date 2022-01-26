05abe1adc371b8be9c7650e38c2aa730ac661c3a
daehan <dh16931@gmail.com>
2022-01-26 17:14:17 +0900

    feat: seperate lang menu button

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,50 +1,24 @@
-import React, {FC, useContext, useEffect} from 'react';
+import React, {FC, useContext} from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
-import LanguageIcon from '@mui/icons-material/Language';
-import Button from '@mui/material/Button';
 
-import { LangContext } from '../context/lang';
+import { LangMenu } from './common';
 
 const pages = ['Home', 'About', 'Projects', 'Blog'];
-const langs = [
-  {
-  value: 'KR',
-  name: '한국어'
-  },
-  {
-  value: 'EN',
-  name: 'English'
-  }
-]
 
 interface HeaderProps {
   handleScroll: (target:string) => void
 }
 
 const Header: FC<HeaderProps> = ({handleScroll}) => {
-  const { state: { language }, dispatch: { setLanguage, translate } } = useContext(LangContext);
-  const [anchorEl, setAnchorEl] = React.useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  }
 
   const handleMenuClick = (page: string) => {
     handleScroll(page)
   }
 
-  const handleLangClick = (lang: string) => {
-    setAnchorEl(null);
-    setLanguage(lang);
-  }
   return (
       <Box sx={{flexGrow: 1}}>
         <AppBar position="static">
@@ -72,31 +46,7 @@ const Header: FC<HeaderProps> = ({handleScroll}) => {
                     }} >{page}</Typography>
                   </MenuItem>
                 ))}
-            <Button
-              id="basic-button"
-              aria-controls={open ? 'basic-menu' : undefined}
-              aria-haspopup="true"
-              aria-expanded={open ? 'true' : undefined}
-              onClick={handleClick}
-              sx={{marginLeft: 'auto'}}
-            >            
-              <LanguageIcon />
-            </Button>
-            <Menu
-              id="basic-menu"
-              anchorEl={anchorEl}
-              open={open}
-              onClose={handleClose}
-            >
-              {langs.map(({value, name})=>(
-                <MenuItem
-                  key={value}
-                  value={value}
-                  onClick={()=>handleLangClick(value)} >
-                  {name}
-                </MenuItem>
-              ))}
-            </Menu>
+            <LangMenu />
           </Toolbar>
         </AppBar>
       </Box>
--- src/components/common/index.tsx
+++ src/components/common/index.tsx
@@ -2,10 +2,13 @@ import BoxWrapper from "./BoxWrapper";
 import BoxHeader from "./BoxHeader";
 import ErrorAlert from "./ErrorAlert";
 import LoadingBox from "./LoadingBox"
+import LangMenu from "./LangMenu"
+
 
 export {
     BoxWrapper,
     BoxHeader,
     ErrorAlert,
-    LoadingBox
+    LoadingBox,
+    LangMenu
 }
